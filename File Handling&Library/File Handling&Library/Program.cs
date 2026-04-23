4473e11740eddbd33ef3c42233db2a114064a511
Abood-essa <aboodessa228@gmail.com>
2026-04-23 14:52:27 +0300

    Interface & Exception - File Handling & Library

---
+++ File Handling&Library/File Handling&Library/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace File_Handling_Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                File.WriteAllText("data.txt", "Abd Al Hameed");
+                File.AppendAllText("data.txt", "\n21 years old");
+
+                string[] lines = File.ReadAllLines("data.txt");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                File.AppendAllText("data.txt", "\nThis is a new line");
+
+                lines = File.ReadAllLines("data.txt");
+                Console.WriteLine("After Appending");
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Interface&Exception/Interface&Exception/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Interface_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator();
+            try
+            {
+                Console.WriteLine("Enter first number to add");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter secound Number");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("the sum is "+c1.Add(a, b));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+
+            }
+
+        }
+    }
+
+    interface ICalculator
+    {
+        int Add(int a, int b);
+    }
+
+    class Calculator : ICalculator
+    {
+
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
